919d88959087cdd7f15d08f4c7bc93a4fb5eda6a
artagaz <and.vaylin@gmail.com>
2025-04-22 14:23:10 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# 1
+# a https://static-maps.yandex.ru/1.x/?ll=86.093537%2C55.350931&z=17&&size=450,450&l=map
+# b https://static-maps.yandex.ru/1.x/?ll=86.134981%2C55.334086&z=17&&size=450,450&l=map
+# c https://static-maps.yandex.ru/1.x/?azimuth=5.497787143782138&ll=86.949773%2C55.003320&z=15&l=map
